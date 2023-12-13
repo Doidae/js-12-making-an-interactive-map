334567e07be97b754d236988dd6f863c6c7c6188
Doidae <andyarredondo1224@gmail.com>
2023-12-12 19:08:46 -0500

    Added select interface

---
+++ main.js
@@ -0,0 +1,5 @@
+function getSelectedOption() {
+    var selectElement = document.getElementById("options");
+    var selectedOption = selectElement.options[selectElement.selectedIndex].value;
+    alert("Selected option: " + selectedOption);
+}
